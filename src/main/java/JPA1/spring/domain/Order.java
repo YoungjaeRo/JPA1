ce26c7fd959dc9aa2be797c9082322dccd9f7030
YoungjaeRo <ryj0330@naver.com>
2024-11-17 14:26:47 +0900

    feat: Order, OrderItem 생성 메서드  & 비즈니스 로직 구현

--- src/main/java/JPA1/spring/domain/Order.java
+++ src/main/java/JPA1/spring/domain/Order.java
@@ -62,4 +62,53 @@ public class Order {
 		delivery.setOrder(this);
 	}
 
+	public void addOrderItem(OrderItem orderItem) {
+		orderItems.add(orderItem);
+		orderItem.setOrder(this);
+	}
+
+	//== 생성 메서드 ==//
+	public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+		Order order = new Order();
+		order.setMember(member);
+		order.setDelivery(delivery);
+		for(OrderItem orderItem : orderItems) {
+			order.addOrderItem(orderItem);
+		}
+		order.setStatus(OrderStatus.ORDER);
+		order.setOrderDate(LocalDateTime.now());
+		return order;
+	}
+
+	// == 비즈니스 로직 == //
+
+	/**
+	 * 1. 주문 취소
+	 */
+
+	public void cancel() {
+		if(delivery.getStatus() == DeliveryStatus.COMP) {
+			throw new IllegalStateException("이미 배송 완룓힌 상품은 취소가 불가능합니다.");
+		} // 이미 배송이 완료된것은 취소 불가
+
+		// 이미 배송이 완료된 것이 아니라면, 취소 진행
+		this.setStatus(OrderStatus.CANCEL);
+		for(OrderItem orderItem : orderItems) { // Order(주문) 에 대한 각각의 OrderItems
+			orderItem.cancel();
+		}
+	}
+
+	// 조회 로직
+	/**
+	 *  전체 주문 금액 조회
+	 */
+	public int getTotalPrice () {
+		int totalPrice = 0;
+		for(OrderItem orderItem : orderItems) {
+			totalPrice = totalPrice + orderItem.getTotalPrice();
+		}
+
+	return totalPrice;
+	}
+
 }
--- src/main/java/JPA1/spring/domain/OrderItem.java
+++ src/main/java/JPA1/spring/domain/OrderItem.java
@@ -30,4 +30,25 @@ public class OrderItem {
 
 	private int count; //  주문당시의 수량
 
+	// 생성 메서드 //
+	public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+		OrderItem orderItem = new OrderItem();
+		orderItem.setItem(item);
+		orderItem.setOrderPrice(orderPrice);
+		orderItem.setCount(count);
+
+		item.removeStock(count); // 주문한 만큼 수량에서 제외
+		return orderItem;
+	}
+
+	// 비즈니스 로직 //
+	public void cancel() {
+		//취소를 했기 때문에, 수량을 취소한 만큼 다시 늘려줌
+		getItem().addStock(count);
+	}
+
+	// 조회 로직 //
+	public int getTotalPrice() {
+		return getOrderPrice() * getCount();
+	}
 }
